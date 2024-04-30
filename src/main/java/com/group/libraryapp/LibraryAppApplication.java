89a0081fda2c7dd5301a99dc56fab9ebd77536ab
Jinseo Jeong <js020315@gmail.com>
2024-04-30 10:45:16 +0900

    Project Setting

--- src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -3,11 +3,11 @@ package com.group.libraryapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 어노테이션
+// Spring 실행 위한 설정을 자동으로 해 줌
 public class LibraryAppApplication {
-
   public static void main(String[] args) {
+    // Spring 서버 시작
     SpringApplication.run(LibraryAppApplication.class, args);
   }
-
 }
